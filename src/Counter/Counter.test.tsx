2c099543dd85873981db6ea132fe8ffc7d872191
choejoonkyung <choejunkyung@gmail.com>
2021-11-09 23:09:35 +0900

    feat: plus, minus fireEvent test code 작성

--- src/Counter/Counter.test.tsx
+++ src/Counter/Counter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Counter from ".";
 
 test("the counter starts at 0", () => {
@@ -17,4 +17,20 @@ test("plus button has correct text", () => {
   render(<Counter />);
   const buttonElement = screen.getByTestId("plus-button");
   expect(buttonElement).toHaveTextContent("+");
+});
+
+test("+ 버튼을 눌렀을 때, 카운터가 1로 변경 된다.", () => {
+  render(<Counter />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent("1");
+});
+
+test("- 버튼을 눌렀을 때, 카운터가 -1로 변경 된다.", () => {
+  render(<Counter />);
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent("-1");
 });
