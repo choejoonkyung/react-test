3860f458cfc8745525e78e8f8e838fcfb5617b8f
choejoonkyung <choejunkyung@gmail.com>
2021-11-11 00:16:41 +0900

    feat: MSW 서버 셋업

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,13 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { server } from "./mocks/server";
+
+// MSW 세팅
+// 모든 테스트 전에 서버를 작동 시킨다.
+// 각각의 테스트가 끝나면 핸들러를 리셋한다.
+// 최종적으로 모든 테스트가 끝난다면 서버를 닫는다.
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
